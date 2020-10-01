e4706e9379255015384c118837799d73a4b67077
Andy Newhouse <hi@andymnewhouse.me>
2020-10-01 13:58:21 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\SalesReport;
+use App\Models\SalesReport;
 
 /*
 |--------------------------------------------------------------------------
